cd814ecb498189f43431183402bcbc8e65fab888
whiskels <kuzmingeo@gmail.com>
2020-06-20 15:22:00 +0300

    Level 36. Finished MVC

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task36.task3608.controller;
+
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
+import com.javarush.task.task36.task3608.view.UsersView;
+
+public class Controller {
+    private Model model;
+    private UsersView usersView;
+    private EditUserView editUserView;
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void onShowAllUsers() {
+        model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onOpenUserEditForm(long userId) {
+        model.loadUserById(userId);
+        editUserView.refresh(model.getModelData());
+    }
+
+    public void onUserDelete(long userId) {
+        model.deleteUserById(userId);
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onUserChange(String name, long id, int level) {
+        model.changeUserData(name, id, level);
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(getAllUsers());
+    }
+
+    public void loadDeletedUsers() {
+        modelData.setDisplayDeletedUserList(true);
+        modelData.setUsers(userService.getAllDeletedUsers());
+    }
+
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+
+    public void deleteUserById(long userId) {
+        userService.deleteUser(userId);
+        loadUsers();
+    }
+
+    public void changeUserData(String name, long id, int level) {
+        userService.createOrUpdateUser(name, id, level);
+        loadUsers();
+    }
+
+    private List<User> getAllUsers() {
+        return userService.filterOnlyActiveUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+}
+
